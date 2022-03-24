9f7763105da4de1a8b33f202390d91fbf29a49e0
Jiyeon31 <zhdzhddlx@gmail.com>
2022-03-24 19:54:25 -0400

    added js files for functions

---
+++ public/javascript/delete-post.js
@@ -0,0 +1,20 @@
+//I will change 'id' names and etc when I get handlebar files
+async function deleteFormHandler(event) {
+    event.preventDefault();
+  
+    const id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+    ];
+    const response = await fetch(`/api/posts/${id}`, {
+      method: 'DELETE'
+    });
+  
+    if (response.ok) {
+      document.location.replace('/dashboard/');
+    } else {
+      alert(response.statusText);
+    }
+  }
+  
+  document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
+  
---
+++ public/javascript/logout.js
@@ -0,0 +1,16 @@
+//I will change 'id' names and etc when I get handlebar files
+async function logout() {
+    const response = await fetch('/api/users/logout', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' }
+    });
+  
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+  }
+  
+  document.querySelector('#logout').addEventListener('click', logout);
+  
