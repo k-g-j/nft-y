521e62a8d8c68f66878f848d5be122ce14d0914c
Jiyeon31 <zhdzhddlx@gmail.com>
2022-03-24 20:00:46 -0400

    added models files

---
+++ models/index.js
@@ -0,0 +1,31 @@
+//I will change foreignKeys and functions when I get seeds files
+// import all models
+const Post = require('./Post');
+const User = require('./User');
+
+// create associations
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+User.belongsToMany(Post, {
+  through: Vote,
+  as: 'voted_posts',
+
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Post.belongsToMany(User, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'post_id',
+  onDelete: 'SET NULL'
+});
+
+module.exports = { User, Post, Vote, Comment };
