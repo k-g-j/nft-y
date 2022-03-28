5ac7161cc215bbc1d3b14b8e9e3e668feb9fd4ff
Kate Johnson <johnson.kat.g@gmail.com>
2022-03-28 00:55:46 -0400

    fixed bug with leave room

--- public/javascript/chat.js
+++ public/javascript/chat.js
@@ -83,7 +83,7 @@ function outputUsers(users) {
 document.getElementById('leave-btn').addEventListener('click', () => {
   const leaveRoom = confirm('Are you sure you want to leave the chatroom?');
   if (leaveRoom) {
-    window.location = '../index.html';
+    window.location.replace('/')
   } else {
   }
 });
