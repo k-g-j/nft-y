f38a4dd35ffad05a23bdd53dd13211a05b0bd316
Lavon Green <lavon_green@yahoo.com>
2022-03-29 23:02:34 -0400

    seed array

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -1,6 +1,23 @@
 const { User } = require('../models');
 const userData = [
- // array of objects here
+    {
+        username: 'NFT guy',
+        email: 'nftguy@email.com',
+        password: 'password',
+        wallet: '0x8a08e3Ce6CED24d376a13C544E45d4DDa02FaFEA',
+      },
+      {
+        username: 'NFT dude',
+        email: 'nftdude@email.com',
+        password: 'password',
+        wallet: '0xD78E2165f190066E16D515f72A7115774D4548b2',
+      },
+      {
+        username: 'NFTina',
+        email: 'nftina@email.com',
+        password: 'password',
+        wallet: '0xA1707c82aa2866955991c7f2C6F431d6619B8b4c',
+      }
 ];
-const seedProjects = () => User.bulkCreate(userData);
+const seedUser = () => User.bulkCreate(userData);
 module.exports = seedUser;
